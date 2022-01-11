4af9d0f44f21540f6c57c71850a4b12f1944ff47
HarshGoyal006 <97098987+HarshGoyal006@users.noreply.github.com>
2022-01-11 10:55:21 +0530

    Add files via upload

---
+++ output1_21105013.py
@@ -0,0 +1,32 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\Dell\OneDrive\Documents\Python Assignment Files\Q1.py ===
+Progr. 1
+enter number 1 :26
+enter number 2 :23
+enter number 3 :12
+The average of given numbers is : 20.333333333333332
+
+==== RESTART: C:\Users\Dell\OneDrive\Documents\Python Assignment Files\Q2.py ===
+Progr. 2
+Number of Dependents :3
+The Gross Income is:4000000
+The Income Tax is : 796200.0
+
+==== RESTART: C:\Users\Dell\OneDrive\Documents\Python Assignment Files\Q3.py ===
+Progr. 3
+[21105013, 'Harsh', 'M', 'ECE', 8.4]
+
+==== RESTART: C:\Users\Dell\OneDrive\Documents\Python Assignment Files\Q4.py ===
+Progr. 4
+Marks without Sort [87, 23, 45, 27, 98, 60]
+Marks with Sort [87, 23, 45, 27, 98, 60]
+
+=== RESTART: C:\Users\Dell\OneDrive\Documents\Python Assignment Files\Q5a.py ===
+Progr. 5a
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+=== RESTART: C:\Users\Dell\OneDrive\Documents\Python Assignment Files\Q5b.py ===
+Progr. 5b
+['Red', 'Green', 'White', 'Purple', 'Yellow']
